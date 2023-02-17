a23bd75c2ac0fb3ccec1ae81a389e4aa26517536
JaredWeakStrike <jddoc31@gmail.com>
2023-02-17 17:02:22 -0500

    just need promise charm to level

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -276,7 +276,7 @@ function GoA()
 --Garden of Assemblage Rearrangement
 if Place == 0x1A04 then
 	--Open Promise Charm Path
-	if ReadByte(Save+0x36B2) > 0 and ReadByte(Save+0x36B3) > 0 and ReadByte(Save+0x36B4) > 0 and ReadByte(Save+0x3694) > 0 then --All Proofs & Promise Charm
+	if ReadByte(Save+0x3694) > 0 then --All Proofs & Promise Charm
 		Spawn('Short',0x06,0x05C,0x77A) --Text
 	end
 	--Demyx's Portal Text
