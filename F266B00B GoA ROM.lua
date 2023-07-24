f206d6a678dc375de43adce9ba837d4daba69623
JaredWeakStrike <jddoc31@gmail.com>
2023-07-24 13:39:39 -0400

    fix local stat increases not being applied if not at max hp

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -639,7 +639,7 @@ while ReadByte(Save+0x3695) > ReadByte(Save+0x35C5) do
 	WriteByte(Save+0x35C5,ReadByte(Save+0x35C5)+1)
 end
 --DUMMY 23 = Maximum HP Increased!
-while ReadByte(Save+0x3671) > ReadByte(Save+0x35CC) and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004)do
+while ReadByte(Save+0x3671) > ReadByte(Save+0x35CC) and ReadInt(Slot1+0x000) > 1 do
 	local Bonus
 	if ReadByte(Save+0x2498) < 3 then --Non-Critical
 		Bonus = 5
@@ -651,7 +651,7 @@ while ReadByte(Save+0x3671) > ReadByte(Save+0x35CC) and ReadInt(Slot1+0x000)== R
 	WriteByte(Save+0x35CC,ReadByte(Save+0x35CC)+1)
 end
 --DUMMY 24 = Maximum MP Increased!
-while ReadByte(Save+0x3672) > ReadByte(Save+0x35CD) and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004) do 
+while ReadByte(Save+0x3672) > ReadByte(Save+0x35CD) and ReadInt(Slot1+0x000) > 1 do 
 	local Bonus
 	if ReadByte(Save+0x2498) < 3 then --Non-Critical
 		Bonus = 10
@@ -663,7 +663,7 @@ while ReadByte(Save+0x3672) > ReadByte(Save+0x35CD) and ReadInt(Slot1+0x000)== R
 	WriteByte(Save+0x35CD,ReadByte(Save+0x35CD)+1)
 end
 --DUMMY 25 = Drive Gauge Powered Up!
-while ReadByte(Save+0x3673) > ReadByte(Save+0x35CE) and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004) do
+while ReadByte(Save+0x3673) > ReadByte(Save+0x35CE) and ReadInt(Slot1+0x000) > 1 do
 	if ReadByte(Slot1+0x1B2) < 9 then
 		WriteByte(Slot1+0x1B1,ReadByte(Slot1+0x1B1)+1)
 		WriteByte(Slot1+0x1B2,ReadByte(Slot1+0x1B2)+1)
@@ -671,21 +671,21 @@ while ReadByte(Save+0x3673) > ReadByte(Save+0x35CE) and ReadInt(Slot1+0x000)== R
 	WriteByte(Save+0x35CE,ReadByte(Save+0x35CE)+1)
 end
 --DUMMY 26 = Gained Armor Slot!
-while ReadByte(Save+0x3674) > ReadByte(Save+0x35D5) and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004) do
+while ReadByte(Save+0x3674) > ReadByte(Save+0x35D5) and ReadInt(Slot1+0x000) > 1 do
 	if ReadByte(Save+0x2500) < 8 then
 		WriteByte(Save+0x2500,ReadByte(Save+0x2500)+1)
 	end
 	WriteByte(Save+0x35D5,ReadByte(Save+0x35D5)+1)
 end
 --DUMMY 27 = Gained Accessory Slot!
-while ReadByte(Save+0x3675) > ReadByte(Save+0x35D6)and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004) do
+while ReadByte(Save+0x3675) > ReadByte(Save+0x35D6)and ReadInt(Slot1+0x000) > 1 do
 	if ReadByte(Save+0x2501) < 8 then
 		WriteByte(Save+0x2501,ReadByte(Save+0x2501)+1)
 	end
 	WriteByte(Save+0x35D6,ReadByte(Save+0x35D6)+1)
 end
 --DUMMY 16 = Gained Item Slot!
-while ReadByte(Save+0x3660) > ReadByte(Save+0x35DB) and ReadInt(Slot1+0x000)== ReadInt(Slot1+0x004) do
+while ReadByte(Save+0x3660) > ReadByte(Save+0x35DB) and ReadInt(Slot1+0x000) > 1 do
 	if ReadByte(Save+0x2502) < 8 then
 		WriteByte(Save+0x2502,ReadByte(Save+0x2502)+1)
 	end
