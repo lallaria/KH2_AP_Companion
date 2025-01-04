19a8b8f48c03f2fe9a8824675934c99fdac493fb
shananas <s.odea@comcast.net>
2025-01-04 00:28:05 -0500

    Version number

--- F266B00B GoA ROM.lua
+++ F266B00B GoA ROM.lua
@@ -7,7 +7,7 @@ LUAGUI_DESC = 'A GoA build for use with the Randomizer. Requires ROM patching.'
 
 function _OnInit()
 GameVersion = 0
-print('GoA v1.54.1 | Archipelago Version V1')
+print('GoA v1.54.2 | Archipelago Version V2')
 GoAOffset = 0x7C
 SeedCleared = false
 end
